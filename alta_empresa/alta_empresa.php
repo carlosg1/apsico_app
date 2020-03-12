51fb2add8c5f222a40ee74de22e5dfe1e9690235
Carlos Garcia <carlosgctes@gmail.com>
2020-03-12 09:46:00 -0300

    carga de datos de nueva empresa

--- alta_empresa/alta_empresa.php
+++ alta_empresa/alta_empresa.php
@@ -4,6 +4,40 @@ require_once('../proxy/conexion.php');
 
 $dato = $_POST;
 
+$qry_id_empresa = "SELECT AUTO_INCREMENT AS id_empresa
+FROM  INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'c1560393_ba2'
+AND   TABLE_NAME   = 'empresa'";
+
+
+try {
+    $rst_id_empresa = $mbd->query($qry_id_empresa);
+
+    $reg_id_empresa = $rst_id_empresa->fetch(PDO::FETCH_OBJ);
+/*
+    foreach($mbd->query($qry_id_empresa) as $fila) {
+        $r1 = $fila[0];
+    }
+*/
+    $a = $reg_id_empresa->id_empresa;
+
+} catch (PDOException $e) {
+    print "Error!!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+
+
+/*
+$query_empresa = "INSERT INTO empresa(id_empresa_tipo, nombre, calle_nombre, calle_nro, pais, provincia, ciudad, telefono) 
+VALUES($reg_id_empresa->id_empresa);";
+
+$query_usuario = "INSERT INTO usuario(correo, id_empresa, id_usuario_tipo, id_especialidad, clave, nombre, apellido)
+VALUES('$dato[\'profesionalEmail\']', $reg_id_empresa->id_empresa);";
+
+$reg_id_empresa = null;
+$rst_id_empresa = null;
+*/
 exit;
 
 
--- proxy/conexion.php
+++ proxy/conexion.php
@@ -2,6 +2,8 @@
 
 try {
     $mbd = new PDO('mysql:host=localhost;dbname=c1560393_ba2', 'c1560393_uxruel4', 'Cl35UGsm39Hjs');
+
+    // $mbd = new PDO('mysql:host=localhost;dbname=c1560393_ba2', 'root', '');
     
 } catch (PDOException $e) {
     print "Error!!: " . $e->getMessage() . "<br/>";
